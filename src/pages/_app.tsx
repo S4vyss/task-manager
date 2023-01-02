b03cb720e52b8e122ba4a6803d574ae8e8282d26
S4vyss <mlodywojownik345@gmail.com>
2023-01-02 17:37:20 +0100

    added header

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,10 @@ import { SessionProvider } from "next-auth/react";
 import { trpc } from "../utils/trpc";
 
 import "../styles/globals.css";
+import '@fontsource/roboto/300.css';
+import '@fontsource/roboto/400.css';
+import '@fontsource/roboto/500.css';
+import '@fontsource/roboto/700.css';
 
 const MyApp: AppType<{ session: Session | null }> = ({
   Component,
