32bf1fac80a4afe435ec51c6689ba13c467ab9fa
S4vyss <mlodywojownik345@gmail.com>
2023-01-15 14:09:50 +0100

    api works

--- src/pages/api/projects.ts
+++ src/pages/api/projects.ts
@@ -1,18 +1,8 @@
 import { type NextApiRequest, type NextApiResponse } from "next";
-import { useSession } from "next-auth/react";
 import { prisma } from "../../server/db/client";
 
-const { data: sessionData } = useSession();
-
 const projects = async (req: NextApiRequest, res: NextApiResponse) => {
-  const data = await prisma.project.findMany({
-    where: {
-      User: {
-        id: sessionData?.user?.id
-      }
-    }
-  });
-  res.status(200).json(data);
+  res.send("api will work sometime")
 };
 
 export default projects;
