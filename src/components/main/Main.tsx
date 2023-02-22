c277d12af88869d61252212aa3154da373be56b6
S4vyss <mlodywojownik345@gmail.com>
2023-02-22 08:23:09 +0100

    changed logo

--- src/components/main/Main.tsx
+++ src/components/main/Main.tsx
@@ -38,87 +38,87 @@ export default function Main(): ReactElement {
   }
 
   return (
-    <Container sx={{ marginLeft: 0, display: "flex", flexWrap: "wrap"}} style={{ maxWidth: "100%", flex: "1 1 auto"}}>
-      {
-        projects.data &&
-          <>
+      <Container sx={{ marginLeft: 0, display: "flex", flexWrap: "wrap"}} style={{ maxWidth: "100%", flex: "1 1 auto"}}>
+        {
+            projects.data &&
+            <>
               <List component="nav" sx={{
                 flexGrow: 1,
                 maxWidth: "25%",
               }}>
                 {projects.data.map(project => {
                   return (
-                    <Box sx={{ display: "flex", justifyContent: "space-between" }} key={project.id}>
-                      <Link href={`/project/[id]`} as={`/project/${project.id}`}
-                            style={{ display: "flex", overflow: "hidden", alignItems: "center", width: "100%" }}
-                      >
-                        <ListItemButton
-                          key={project.id}
-                          sx={{ display: "flex", alignItems: "center", gap: 1}}
+                      <Box sx={{ display: "flex", justifyContent: "space-between" }} key={project.id}>
+                        <Link href={`/project/[id]`} as={`/project/${project.id}`}
+                              style={{ display: "flex", overflow: "hidden", alignItems: "center", width: "100%" }}
                         >
-                          <ListItemIcon sx={{ minWidth: "fit-content" }}>
-                            <AutoAwesomeMosaicIcon />
+                          <ListItemButton
+                              key={project.id}
+                              sx={{ display: "flex", alignItems: "center", gap: 1}}
+                          >
+                            <ListItemIcon sx={{ minWidth: "fit-content" }}>
+                              <AutoAwesomeMosaicIcon />
+                            </ListItemIcon>
+                            <ListItemText primary={project.title} style={{textOverflow: "ellipsis", whiteSpace: "nowrap", overflow: "hidden"}} />
+                          </ListItemButton>
+                        </Link>
+                        <ListItemButton
+                            sx={{ maxWidth: "fit-content" }}
+                        >
+                          <ListItemIcon sx={{ minWidth: "fit-content" }} onClick={() => handleDelete(project.id)}>
+                            <DeleteIcon />
                           </ListItemIcon>
-                          <ListItemText primary={project.title} style={{textOverflow: "ellipsis", whiteSpace: "nowrap", overflow: "hidden"}} />
                         </ListItemButton>
-                      </Link>
-                      <ListItemButton
-                        sx={{ maxWidth: "fit-content" }}
-                      >
-                        <ListItemIcon sx={{ minWidth: "fit-content" }} onClick={() => handleDelete(project.id)}>
-                          <DeleteIcon />
-                        </ListItemIcon>
-                      </ListItemButton>
-                    </Box>
+                      </Box>
                   )
                 })}
               </List>
-          </>
-      }
-      <Box sx={{
-        flexGrow: 1,
-        display: "flex",
-        flexDirection: "column",
-        justifyContent: "center",
-        alignItems: "center",
-      }}>
+            </>
+        }
         <Box sx={{
-          backgroundColor: "white",
-          padding: 6,
-          boxShadow: "0px 0px 25px 1px rgba(66, 68, 90, 1)",
-          borderRadius: 2
+          flexGrow: 1,
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "center",
+          alignItems: "center",
         }}>
-          <h1 className="h1" style={{
-            fontFamily: "Roboto,Helvetica,Arial,sans-serif",
-            textTransform: "uppercase",
-            textAlign: "center",
-            margin: 16
+          <Box sx={{
+            backgroundColor: "white",
+            padding: 6,
+            boxShadow: "0px 0px 25px 1px rgba(66, 68, 90, 1)",
+            borderRadius: 2
           }}>
-            Create project
-          </h1>
-          <form onSubmit={handleCreate} style={{ maxWidth: "400px" }}>
-            <FormControl sx={{
-              display: "flex",
-              flexDirection: "column",
-              gap: 3,
+            <h1 className="h1" style={{
+              fontFamily: "Roboto,Helvetica,Arial,sans-serif",
+              textTransform: "uppercase",
+              textAlign: "center",
+              margin: 16
             }}>
-              <TextField
-                type="text"
-                variant="standard"
-                value={title}
-                onChange={(e: any) => setTitle(e.target.value)}
-                label="Title:"
-                sx={{ minWidth: "350px" }}
-                inputProps={{ maxLength: 20}}
-                required={true}
-              />
-              <Button variant="contained" type="submit" sx={{ width: "fit-content", alignSelf: "center" }}>
-                Create
-              </Button>
-            </FormControl>
-          </form>
+              Create project
+            </h1>
+            <form onSubmit={handleCreate} style={{ maxWidth: "400px" }}>
+              <FormControl sx={{
+                display: "flex",
+                flexDirection: "column",
+                gap: 3,
+              }}>
+                <TextField
+                    type="text"
+                    variant="standard"
+                    value={title}
+                    onChange={(e: any) => setTitle(e.target.value)}
+                    label="Title:"
+                    sx={{ minWidth: "350px" }}
+                    inputProps={{ maxLength: 20}}
+                    required={true}
+                />
+                <Button variant="contained" type="submit" sx={{ width: "fit-content", alignSelf: "center" }}>
+                  Create
+                </Button>
+              </FormControl>
+            </form>
+          </Box>
         </Box>
-      </Box>
-    </Container>
+      </Container>
   )
 }
